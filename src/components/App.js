d63fbbfac7c2c00a48519edebc0b475e683a0b70
Kevin Chen <xkevinchen@gmail.com>
2019-08-26 13:54:51 -0400

    Minor edits

--- src/components/App.js
+++ src/components/App.js
@@ -32,7 +32,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => {
   return {
     requestAlerts: () => dispatch(requestAlerts()),
-  };
-};
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
